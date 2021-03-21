5084298cd269468499d723daefb9eeb3db387114
Bruno D'Luka <45696119+bdlukaa@users.noreply.github.com>
2021-03-21 20:46:10 -0300

    Version 3.1.0. Read the changelog

--- lib/src/scripts/extensions.dart
+++ lib/src/scripts/extensions.dart
@@ -3,7 +3,9 @@
 extension Str on List<dynamic> {
   List<String> toStringList() {
     final l = <String>[];
-    for (var i in this) l.add(i.toString());
+    for (var i in this) {
+      l.add(i.toString());
+    }
     return l;
   }
 }
