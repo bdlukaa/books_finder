b2ecacefeaeeda35e6c18ffa226a81ce3583b408
Bruno D'Luka <brunodlukaa@gmail.com>
2022-07-17 19:26:52 -0300

    Use DateTime.tryParse instead of parsing each element

--- lib/src/scripts/books.dart
+++ lib/src/scripts/books.dart
@@ -213,35 +213,8 @@ class BookInfo {
     Map<String, dynamic> json, {
     bool reschemeImageLinks = false,
   }) {
-    final publishedDateArray =
-        ((json['publishedDate'] as String?) ?? '0000-00-00').split('-');
-
-    // initialize datetime variable
-    DateTime? publishedDate;
-    if (publishedDateArray.isNotEmpty) {
-      // initialize date
-      int year = int.parse(publishedDateArray[0]);
-      int month = 1;
-      int day = 1;
-
-      // now test the date string
-      if (publishedDateArray.length == 1) {
-        // assume we have only the year
-        year = int.parse(publishedDateArray[0]);
-      }
-      if (publishedDateArray.length == 2) {
-        // assume we have the year and maybe the month (this could be just a speculative case)
-        year = int.parse(publishedDateArray[0]);
-        month = int.parse(publishedDateArray[1]);
-      }
-      if (publishedDateArray.length == 3) {
-        // assume we have year-month-day
-        year = int.parse(publishedDateArray[0]);
-        month = int.parse(publishedDateArray[1]);
-        day = int.parse(publishedDateArray[2]);
-      }
-      publishedDate = DateTime(year, month, day);
-    }
+    DateTime? publishedDate =
+        DateTime.tryParse((json['publishedDate'] as String?) ?? '0000-00-00');
 
     final imageLinks = <String, Uri>{};
     (json['imageLinks'] as Map<String, dynamic>?)?.forEach((key, value) {
